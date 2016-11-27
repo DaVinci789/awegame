6fbb9483418dd009370b048b69f07082e8990769
DaVinci789 <jaden.manacsa1228@gmail.com>
2016-11-27 23:39:47 +0000

    Add logic to main_menu
    - TODO: Add world scene to switch to.

---
+++ scripts/mm_buttons.gd
@@ -0,0 +1,16 @@
+extends HBoxContainer
+
+onready var start_button = get_node("start")
+onready var quit_button  = get_node("quit")
+
+func _ready():
+	start_button.connect("pressed", self, "_start_game")
+	quit_button.connect("pressed", self, "_quit_game")
+
+func _start_game():
+	get_tree().change_scene("")
+	self.queue_free()
+
+func _quit_game():
+	get_tree().quit()
+	self.queue_free()
