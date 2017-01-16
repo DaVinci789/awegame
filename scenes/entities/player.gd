fc3d51882824b5468e1bec43dd42999d95529510
DaVinci789 <jaden.manacsa1228@gmail.com>
2017-01-16 23:46:16 +0000

    Add random exclusion function
    - Add debug line trail connecting the runner and player node

--- scenes/entities/player.gd
+++ scenes/entities/player.gd
@@ -10,6 +10,7 @@ func _ready():
 	set_process(true)
 
 func _process(delta):
+	update()
 	globals.player_node = self
 	last_animation = current_animation
 	if (Input.is_action_pressed("ui_down")):
@@ -32,3 +33,11 @@ func _process(delta):
 		#print(current_animation)
 		anim.set_speed(2)
 		anim.play(current_animation)
+
+func _draw():
+	#draw_line(Vector2(0,0), Vector2(atan(globals.runner_node.get_pos()[0] - self.get_pos()[0]) * 50, atan(globals.runner_node.get_pos()[1] - self.get_pos()[1] * 50)), Color(255, 0, 0), 1)
+	var rpos = globals.runner_node.get_global_pos()
+	var self_gtrans = self.get_global_transform()
+	var plocal = self_gtrans.xform_inv(rpos)
+	
+	draw_line(Vector2(0,0), plocal, Color(255, 0, 0))
--- scenes/entities/runner.gd
+++ scenes/entities/runner.gd
@@ -6,7 +6,7 @@ extends KinematicBody2D
 var speed
 export (int) var speed_vals_min = 10
 export (int) var speed_vals_max = 15
-export (int) var teleport_range = 150
+export (int) var teleport_range = 400
 
 onready var runner_area = get_node("runner_area")
 onready var runner_tele = get_node("runner_teleport")
@@ -22,7 +22,6 @@ func _ready():
 
 func _process(delta):
 	#print(speed)
-	
 	if (Input.is_action_pressed("ui_up")):
 		move(Vector2(0, -speed))
 	elif (Input.is_action_pressed("ui_down")):
@@ -39,8 +38,13 @@ func _on_area_hit(body):
 
 func _on_tele_start():
 	randomize()
-	var tele_x = rand_range(-teleport_range, teleport_range)
-	var tele_y = rand_range(-teleport_range, teleport_range)
+	#var tele_x = floor(rand_range(400, teleport_range))
+	var tele_x = randex(range(-100, 100))
+	print(tele_x)
+	#var tele_y = floor(rand_range(400, teleport_range))
+	var tele_y = randex(range(-100, 100))
+	print(tele_y)
+
 	if (globals.distance_to(self, globals.player_node) <= teleport_range):
 		# Use a variable here for teleport_range?
 		self.set_pos(Vector2(self.get_pos()[0] + tele_x, \
@@ -50,3 +54,18 @@ func _on_tele_start():
 	else:
 		print("GAME OVER")
 		get_tree().set_pause(true)
+
+func randex(exclude):
+	# Take a random number, R, and map it against the amount of EXCLUDEd numbers.
+	# This results in getting a random number that is not in EXCLUDE.
+	# This function also only works when the exclude range is exactly in the middle of
+	# random numbers generated.
+	randomize()
+	var r = floor(rand_range(-teleport_range, teleport_range - exclude.size()))
+	print("foop: " + str(teleport_range))
+	#while r in exclude:
+	#	r += 1
+	print("r is " + str(r))
+	if r > -100:
+		r += exclude.size()
+	return r
